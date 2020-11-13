0b0a90930f8ddc5dfe1d0baea4d5c318f7645bf3
Josephhoang <71840400+Josephhoang@users.noreply.github.com>
2020-11-12 19:56:39 -0500

    Create pause.py

---
+++ pause.py
@@ -0,0 +1,21 @@
+global pause
+    while pause:
+            for event in pygame.event.get():
+                if event.type==pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                    sys.exit()
+            gamedisplays.blit(instruction_background,(0,0))
+            largetext=pygame.font.Font('freesansbold.ttf',115)
+            TextSurf,TextRect=text_objects("PAUSED",largetext)
+            TextRect.center=((display_width/2),(display_height/2))
+            gamedisplays.blit(TextSurf,TextRect)
+            button("CONTINUE",150,450,150,50,green,bright_green,"unpause")
+            button("RESTART",350,450,150,50,blue,bright_blue,"play")
+            button("MAIN MENU",550,450,150,50,red,bright_red,"menu")
+            pygame.display.update()
+            clock.tick(30)
+
+def unpaused():
+    global pause
+    pause=False
